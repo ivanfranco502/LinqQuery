6b84dabdf8f0ed1408192296f5b93be307d21408
Ivan Franco <ifranco@tupaca.com>
2019-08-20 15:01:26 -0300

    first commit - project creation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
